4e4f84c1582f0aae2219e4792db93e56c62288d3
jungeun-p <61823846+jungeun-p@users.noreply.github.com>
2020-09-15 22:35:15 +0900

    update form

--- react-study/src/components/UpdateContent.js
+++ react-study/src/components/UpdateContent.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class UpdateContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.data.title,
+      desc: this.props.data.desc,
+    };
+    this.inputFormHandler = this.inputFormHandler.bind(this);
+  }
+  inputFormHandler(e) {
+    //중복제거
+    this.setState({ [e.target.name]: e.target.value });
+  }
   render() {
     console.log(this.props.data);
     console.log('updateContent rendering');
@@ -16,9 +28,20 @@ class UpdateContent extends Component {
           }.bind(this)}
         >
           <p>
-            <input type='text' name='title' placeholder='title'></input>
+            <input
+              type='text'
+              name='title'
+              placeholder='title'
+              value={this.state.title}
+              onChange={this.inputFormHandler}
+            ></input>
           </p>
-          <textarea name='desc' placeholder='description'></textarea>
+          <textarea
+            name='desc'
+            placeholder='description'
+            value={this.state.desc}
+            onChange={this.inputFormHandler}
+          ></textarea>
           <p>
             <input type='submit' value='submit'></input>
           </p>
